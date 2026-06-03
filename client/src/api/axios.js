027997ba3f6ef2ca1f5b72485f7bda99ccfd56dd
suraxj <prakashsinghsuraj69@gmail.com>
2026-06-03 11:54:29 +0530

    commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
